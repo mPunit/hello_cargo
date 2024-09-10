1aa06c28c3b3b32ca0d6e3d09dccb22b2112b26f
Punit <punitramyadav@gmail.com>
2024-09-10 19:07:37 -0400

    TESTING

---
+++ sample_app/target/debug/deps/sample_app-2a7257c29e393e2d.d
@@ -0,0 +1,5 @@
+/Users/puntiramyadav/projects/hello_cargo/sample_app/target/debug/deps/sample_app-2a7257c29e393e2d: src/main.rs
+
+/Users/puntiramyadav/projects/hello_cargo/sample_app/target/debug/deps/sample_app-2a7257c29e393e2d.d: src/main.rs
+
+src/main.rs:
---
+++ sample_app/target/debug/sample_app.d
@@ -0,0 +1,1 @@
+/Users/puntiramyadav/projects/hello_cargo/sample_app/target/debug/sample_app: /Users/puntiramyadav/projects/hello_cargo/sample_app/src/main.rs
