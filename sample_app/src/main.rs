7d95f0e37736f35231b7ab5d5f89875b2323c84b
Punit <punitramyadav@gmail.com>
2024-09-29 17:56:05 -0400

    Box Poiner

--- sample_app/src/main.rs
+++ sample_app/src/main.rs
@@ -1,13 +1,9 @@
-use std::{cell::RefCell, rc::Rc};
-
 fn main() {
-    // Rc pointer lets multiple immutable ownership
-    // While RefCell pointer at the same time allows us to mutate the internal value.
-    // So with combination of both, you can have one data that will havemultiple owners and
-    // can change its data as well
-    let a = Rc::new(RefCell::new(String::from("abc")));
-    let b = Rc::clone(&a);
+    let a = Box::new(String::from("ABCD"));
 
-    *b.borrow_mut() = String::from("Hello");
-    println!("{:?}", a)
+    hello(&a);
+}
+
+fn hello(name: &str) {
+    println!("{name}");
 }
