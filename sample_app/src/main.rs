c5c9eb6aa64a822a1ec5cd6ef6346d85ee7be003
Punit <punitramyadav@gmail.com>
2024-10-07 13:41:11 -0400

    bytes to utf-8 scalar values

--- sample_app/src/main.rs
+++ sample_app/src/main.rs
@@ -1,9 +1,28 @@
+use core::str;
+
 fn main() {
-    let a = Box::new(String::from("ABCD"));
+    let bytestring1: &[u8; 24] = b"thi \xE3\x82\x88 is a byte string";
+    let bytestring2: &[u8; 26] = b"thi \xE3 \x82 \x88 is a byte string";
 
-    hello(&a);
-}
+    println!("bytestring1 = {:?}", bytestring1);
+    match str::from_utf8(bytestring1) {
+        Ok(s) => println!("Successfull: {}", s),
+        Err(e) => println!("Unsuccessfull: {}", e),
+    }
 
-fn hello(name: &str) {
-    println!("{name}");
+    println!("");
+
+    println!("bytestring2 = {:?}", bytestring2);
+    match str::from_utf8(bytestring2) {
+        Ok(s) => println!("Successfull: {}", s),
+        Err(e) => println!("Unsuccessfull: {}", e),
+    }
+
+    /* OUTPUT
+    bytestring1 = [116, 104, 105, 32, 227, 130, 136, 32, 105, 115, 32, 97, 32, 98, 121, 116, 101, 32, 115, 116, 114, 105, 110, 103]
+    Successfull: thi よ is a byte string
+
+    bytestring2 = [116, 104, 105, 32, 227, 32, 130, 32, 136, 32, 105, 115, 32, 97, 32, 98, 121, 116, 101, 32, 115, 116, 114, 105, 110, 103]
+    Unsuccessfull: invalid utf-8 sequence of 1 bytes from index 4
+    */
 }
--- sample_app/target/debug/deps/sample_app-5cb18e618aa9bc7b.d
+++ sample_app/target/debug/deps/sample_app-5cb18e618aa9bc7b.d
@@ -1,5 +1,5 @@
-/Users/puntiramyadav/projects/hello_cargo/sample_app/target/debug/deps/libsample_app-5cb18e618aa9bc7b.rmeta: src/main.rs
+/Users/puntiramyadav/Personal_projects/hello_cargo/sample_app/target/debug/deps/libsample_app-5cb18e618aa9bc7b.rmeta: src/main.rs
 
-/Users/puntiramyadav/projects/hello_cargo/sample_app/target/debug/deps/sample_app-5cb18e618aa9bc7b.d: src/main.rs
+/Users/puntiramyadav/Personal_projects/hello_cargo/sample_app/target/debug/deps/sample_app-5cb18e618aa9bc7b.d: src/main.rs
 
 src/main.rs:
